932630f2f2bd6cde6ba9ca8200fa345d3ca34fc6
sinsail <suni1011shah@gmail.com>
2024-02-02 10:52:57 +0545

    Question Solved

---
+++ SQRT_OF_NUMBER.py
@@ -0,0 +1,8 @@
+# Python Program to Find the Square Root
+
+n  = float(input("Enter a number: "))
+
+def sqrt(number):
+    print(f"The square root of the {number} is {number**0.5:.3f} ")
+
+sqrt(n)
---
+++ Sum_number.py
@@ -0,0 +1,6 @@
+# Python Program to Add Two Numbers
+def sum(a,b):
+    print(f"The sum of the number {a} and {b} is {a+b}")
+
+sum(9,14)
+
