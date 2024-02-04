1103e09b2c95d3f63c6bf757798c1bf8cd0edfbb
sinsail <suni1011shah@gmail.com>
2024-02-04 17:44:00 +0545

    Comment removed

--- Lists.py
+++ Lists.py
@@ -1,83 +1,83 @@
-# '''3-1. Names: Store the names of a few of your friends in a list called names. Print
-# each person’s name by accessing each element in the list, one at a time.'''
+'''3-1. Names: Store the names of a few of your friends in a list called names. Print
+each person’s name by accessing each element in the list, one at a time.'''
 
-# lists = ['Sushil', 'Sunil', 'Srijan', 'Prashant','']
-# for name in lists:
-#     print(name)
+lists = ['Sushil', 'Sunil', 'Srijan', 'Prashant','']
+for name in lists:
+    print(name)
 
-# '''3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
-# printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the
-# person’s name.'''
+'''3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the
+person’s name.'''
 
-# lists = ['Sushil', 'Sunil', 'Srijan', 'Prashant','Ram']
-# for name in lists:
-#     print(f"Hello {name}")
-
-
-
-# """3-3. Your Own List: Think of your favorite mode of transportation, such as a
-# motorcycle or a car, and make a list that stores several examples. Use your list
-# to print a series of statements about these items, such as “I would like to own a
-# Honda motorcycle.”
-# """
-# lists = ['Car', 'Bike', 'Bus', 'Aeroplane']
-# print(f'I would like to own a Honda {lists[-3]}')
-
-
-# '''The following exercises are a bit more complex than those in Chapter 2, but
-# they give you an opportunity to use lists in all of the ways described.
-
-# 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
-# would you invite? Make a list thatt least includes a three people you’d like to
-# invite to dinner. Then use your list to print a message to each person, inviting
-# them to dinner.'''
-
-# lists = ['Sushil', 'Sunil', 'Samir']
-# for name in lists:
-#     print(name)
-#     print(f"\tI would like to invide {name} to dinnner.")
+lists = ['Sushil', 'Sunil', 'Srijan', 'Prashant','Ram']
+for name in lists:
+    print(f"Hello {name}")
 
 
 
-
-# """3-5. Changing Guest List: You just heard that one of your guests can’t make the
-# dinner, so you need to send out a new set of invitations. You’ll have to think of
-# someone else to invite.
-# • Start with your program from Exercise 3-4. Add a print() call at the end of
-# your program, stating the name of the guest who can’t make it.
-# • Modify your list, replacing the name of the guest who can’t make it with the
-# name of the new person you are inviting.
-# • Print a second set of invitation messages, one for each person who is still in
-# your list.
-# """
-# lists = ['Sushil', 'Sunil', 'Samir']
-# who_cant_makeittodinner = 'Sunil'
-# lists.remove('Sunil')
-# lists.append('Ranjit')
-# for name in lists:
-#     print(name)
-#     print(f"\tI would like to invide {name} to dinnner.")
+"""3-3. Your Own List: Think of your favorite mode of transportation, such as a
+motorcycle or a car, and make a list that stores several examples. Use your list
+to print a series of statements about these items, such as “I would like to own a
+Honda motorcycle.”
+"""
+lists = ['Car', 'Bike', 'Bus', 'Aeroplane']
+print(f'I would like to own a Honda {lists[-3]}')
 
 
-# '''3-6. More Guests: You just found a bigger dinner table, so now more space is
-# available. Think of three more guests to invite to dinner.
-# • Start with your program from Exercise 3-4 or 3-5. Add a print() call to the
-# end of your program, informing people that you found a bigger table.
-# • Use insert() to add one new guest to the beginning of your list.
-# • Use insert() to add one new guest to the middle of your list.
-# • Use append() to add one new guest to the end of your list.
-# • Print a new set of invitation messages, one for each person in your list.'''
+'''The following exercises are a bit more complex than those in Chapter 2, but
+they give you an opportunity to use lists in all of the ways described.
 
-# lists = ['Sushil', 'Sunil', 'Samir']
-# who_cant_makeittodinner = 'Sunil'
-# lists.remove('Sunil')
-# lists.append('Ranjit')
-# lists.insert(2, 'Suman')
-# lists.insert(0, "Ramey")
-# lists.append('Syamey')
-# for name in lists:
-#     print(name)
-#     print(f"\tI would like to invide {name} to dinnner.")
+3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+would you invite? Make a list thatt least includes a three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting
+them to dinner.'''
+
+lists = ['Sushil', 'Sunil', 'Samir']
+for name in lists:
+    print(name)
+    print(f"\tI would like to invide {name} to dinnner.")
+
+
+
+
+"""3-5. Changing Guest List: You just heard that one of your guests can’t make the
+dinner, so you need to send out a new set of invitations. You’ll have to think of
+someone else to invite.
+• Start with your program from Exercise 3-4. Add a print() call at the end of
+your program, stating the name of the guest who can’t make it.
+• Modify your list, replacing the name of the guest who can’t make it with the
+name of the new person you are inviting.
+• Print a second set of invitation messages, one for each person who is still in
+your list.
+"""
+lists = ['Sushil', 'Sunil', 'Samir']
+who_cant_makeittodinner = 'Sunil'
+lists.remove('Sunil')
+lists.append('Ranjit')
+for name in lists:
+    print(name)
+    print(f"\tI would like to invide {name} to dinnner.")
+
+
+'''3-6. More Guests: You just found a bigger dinner table, so now more space is
+available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the
+end of your program, informing people that you found a bigger table.
+• Use insert() to add one new guest to the beginning of your list.
+• Use insert() to add one new guest to the middle of your list.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list.'''
+
+lists = ['Sushil', 'Sunil', 'Samir']
+who_cant_makeittodinner = 'Sunil'
+lists.remove('Sunil')
+lists.append('Ranjit')
+lists.insert(2, 'Suman')
+lists.insert(0, "Ramey")
+lists.append('Syamey')
+for name in lists:
+    print(name)
+    print(f"\tI would like to invide {name} to dinnner.")
 
 '''3-7. Shrinking Guest List: You just found out that your new dinner table won’t
 arrive in time for the dinner, and now you have space for only two guests.
@@ -114,65 +114,67 @@ for name in lists:
 
 del lists[0:]
 print(lists)
-# # Counting to Twenty: Use a for loop to print the numbers from 1 to 20,inclusive.
-
-# for i in range(1,21):
-#     print(i)
 
 
-# '''Make a list of the numbers from one to one million, and then
-# use a for loop to print the numbers. (If the output is taking too long, stop it by
-# pressing CTRL-C or by closing the output window.'''
+# Counting to Twenty: Use a for loop to print the numbers from 1 to 20,inclusive.
 
-# number = list(range(1, 1000001))
-# for i in number:
-#     print(i)
+for i in range(1,21):
+    print(i)
 
 
+'''Make a list of the numbers from one to one million, and then
+use a for loop to print the numbers. (If the output is taking too long, stop it by
+pressing CTRL-C or by closing the output window.'''
 
-# '''Summing a Million: Make a list of the numbers from one to one million, and
-# then use min() and max() to make sure your list actually starts at one and ends
-# at one million. Also, use the sum() function to see how quickly Python can add
-# a million numbers'''
-
-# number = [i for i in range(1,1000001)]
-# print(min(number))
-# print(max(number))
-# print(sum(number))
+number = list(range(1, 1000001))
+for i in number:
+    print(i)
 
 
-# '''Use the third argument of the range() funcmbetion to make a list
-# of the odd numbers from 1 to 20. Use a for loop to print each number.'''
 
-# a = [i for i in range(1,21,2)]
-# for b in a:
-#     print(b)
+'''Summing a Million: Make a list of the numbers from one to one million, and
+then use min() and max() to make sure your list actually starts at one and ends
+at one million. Also, use the sum() function to see how quickly Python can add
+a million numbers'''
 
-# """ Make a list of the multiples of 3, from 3 to 30. Use a for loop to
-# print the numbers in your list."""
-
-# list = []
-# for i in range(1,31):
-#     if i%3==0:
-#         list.append(i)
-# print(list)
-
-# # By using list comprehension for same quesiton
-# list = [i for i in range(1,31) if i%3==0]
-# print(list)
-
-# '''A number raised to the third power is called a cube. For example,
-# the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that
-# is, the cube of each integer from 1 through 10), and use a for loop to print out
-# the value of each cube'''
-
-# list  = [i**3 for i in range(1,11)]
-# for j in list:
-#     print(j)
+number = [i for i in range(1,1000001)]
+print(min(number))
+print(max(number))
+print(sum(number))
 
 
-# '''Cube Comprehension: Use a list comprehension to generate a list of the first
-# 10 cubes.'''
+'''Use the third argument of the range() funcmbetion to make a list
+of the odd numbers from 1 to 20. Use a for loop to print each number.'''
 
-# list = [i**3 for i in range(1,11)]
-# print(list)
+a = [i for i in range(1,21,2)]
+for b in a:
+    print(b)
+
+""" Make a list of the multiples of 3, from 3 to 30. Use a for loop to
+print the numbers in your list."""
+
+list = []
+for i in range(1,31):
+    if i%3==0:
+        list.append(i)
+print(list)
+
+# By using list comprehension for same quesiton
+list = [i for i in range(1,31) if i%3==0]
+print(list)
+
+'''A number raised to the third power is called a cube. For example,
+the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes (that
+is, the cube of each integer from 1 through 10), and use a for loop to print out
+the value of each cube'''
+
+list  = [i**3 for i in range(1,11)]
+for j in list:
+    print(j)
+
+
+'''Cube Comprehension: Use a list comprehension to generate a list of the first
+10 cubes.'''
+
+list = [i**3 for i in range(1,11)]
+print(list)
